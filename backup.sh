f3a4a9b0cb8e82734a3ab00bcb9ccec01947dc6d
Ulhas Mandrawadkar <ulhas.sm@gmail.com>
2021-10-21 10:25:05 +0530

    Fix copy script for lua folder

--- backup.sh
+++ backup.sh
@@ -6,7 +6,7 @@ cp ~/.skhdrc yabai/.skhdrc
 
 # vim
 cp ~/.config/nvim/init.vim vim/nvim/init.vim
-cp -R ~/.config/nvim/lua vim/lua
+cp -R ~/.config/nvim/lua vim/nvim
 
 # fish
 cp ~/.config/fish/config.fish fish/config.fish
--- restore.sh
+++ restore.sh
@@ -6,7 +6,7 @@ cp yabai/.skhdrc ~/.skhdrc
 
 # vim
 cp vim/nvim/init.vim ~/.config/nvim/init.vim
-cp -R vim/lua ~/.config/nvim
+cp -R vim/nvim/lua ~/.config/nvim
 
 # fish
 cp fish/config.fish ~/.config/fish/config.fish
